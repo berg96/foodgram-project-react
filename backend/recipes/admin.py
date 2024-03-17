2cd287849940bca96076d7a1b37e43ec6d3eabd3
Artem Kulikov <chigar2010@yandex.ru>
2024-03-17 20:50:56 +0200

    add model Ingredient

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    measurement_unit = models.CharField(
+        max_length=200, verbose_name='Единица измерения'
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
